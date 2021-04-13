29ded1483b992e0ab510c8e52cce31274bb7b995
Phat Giang <phatgiang1995@gmail.com>
2021-04-13 14:27:28 -0700

    Phase 3

--- test.js
+++ test.js
@@ -1,10 +1,28 @@
 const fetch = require('node-fetch');
 
-fetch('https://swapi.dev/api/people/1')
+const getPerson = id => {
+return fetch(`https://swapi.dev/api/people/${id}`, {})
     .then(response => response.json())
-    .then(person => console.log(person));
+// .then(person => console.log(person))
+}
 
-    Promise.all(promises)
-        .then((result) => {
+const getHomeWorld = person => {
+    const homeWorld = person.homeworld;
+    return fetch(homeWorld)
+    .then(response => response.json())
+    // .then(json => console.log(json))
+}
 
-        })
+const getFilm = person => {
+    const film = Promise.all(person.films);
+    return fetch(film)
+    .then(response => response.json())
+}
+
+getPerson(1)
+    .then(response => getHomeWorld(response)
+    .then((response2) => console.log(response2)))
+
+getPerson(1)
+    .then(response => getFilm(response))
+    .then(response2 => console.log(response2))
